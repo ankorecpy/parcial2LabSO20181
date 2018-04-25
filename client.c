b222d47036b5d2974d0191a967c8a4424fc5ed6f
ankore <ankorecpy@outlook.com>
2018-04-25 16:13:43 -0500

    Se implementa el envio y recepcion de archivos

--- client.c
+++ client.c
@@ -6,7 +6,11 @@
 #include <sys/socket.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <fcntl.h>
 #include <unistd.h>
+#include <string.h>
+
+#define BUFFER_SIZE 255
 
 void put_file(int sockfd, char * path, char * server_path);
 void get_file(int sockfd, char * server_path);
@@ -15,6 +19,7 @@ struct cabecera_transaccion {
 	int tipo;
 	int tamanio;
 	char ruta[150];
+  char nombreArchivo[50];
 };
 
 typedef struct cabecera_transaccion transaccion;
@@ -51,11 +56,7 @@ int main(int argc, char * argv[]) {
   if (connect(s, info->ai_addr, info->ai_addrlen) != 0) {
     perror("bind");
     exit(EXIT_FAILURE);
-  }
-
-	put_file(s, NULL, NULL);//BORRAR LINEA DE PRUEBA
-	
-	
+  }	
   //armar el encabezado y enviar al servidor write(s, ...)
 
   //si argv[1] es "put"
@@ -65,24 +66,55 @@ int main(int argc, char * argv[]) {
   //si argv[1] es "get"
     //server_path = argv[3]
     // get_file(s, server_path);
+    int accionEncontrada = 0;
+    if (strcmp(argv[1], "put") == 0) {
+      if (argc > 4) {
+        put_file(s, argv[3], argv[4]);
+      } else {
+        put_file(s, argv[3], "");
+      }      
+      accionEncontrada = 1;
+    }
+    if (strcmp(argv[1], "get") == 0) {
+      accionEncontrada = 1;
+    }
+    if (!accionEncontrada) {
+      fprintf(stderr, "\nOpcion invalida.");
+      exit(EXIT_FAILURE);
+    }
 
   close(s);
-
    exit(EXIT_SUCCESS);
 }
 
 void put_file(int sockfd, char * path, char * server_path) {
-	
-	//LINEAS DE PRUEBA
-	
+	struct stat st;
+  char buffer[BUFFER_SIZE];
 	transaccion cabecera;
+  int bytesLeidos, fdArchivo;
+  printf("\npath: %s", path);
+  fflush(stdout);
+  if (stat(path, &st) != 0) {
+    perror("stat");
+    exit(EXIT_FAILURE);    
+  }
 	cabecera.tipo = 1;
-	cabecera.tamanio = 15;
-	strcpy(cabecera.ruta, "ninguna");
-	
-	//BORRAR EN CUEANTO SE PUEDA LAS LINEAS ANTERIORES
-	
+	cabecera.tamanio = st.st_size;
+	strcpy(cabecera.ruta, server_path);
+  strcpy(cabecera.nombreArchivo, path);	
 	write(sockfd, &cabecera, sizeof(transaccion));
+  fdArchivo = open(path, O_RDONLY);
+  if (fdArchivo == -1) {
+    perror("open");
+    exit(EXIT_FAILURE);
+  }
+  do {
+    bytesLeidos = read(fdArchivo, buffer, BUFFER_SIZE);
+    if (bytesLeidos > 0) {
+      write(sockfd, buffer, bytesLeidos);
+    }
+  } while (bytesLeidos > 0);
+
 }
 void get_file(int sockfd, char * server_path) {
 	
--- server.c
+++ server.c
@@ -6,12 +6,18 @@
 #include <sys/socket.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <fcntl.h>
 #include <unistd.h>
+#include <string.h>
+
+#define BUFFER_SIZE 255
+#define TAM_CONFIG_PATH 50
 
 struct cabecera_transaccion {
 	int tipo;
 	int tamanio;
 	char ruta[150];
+  char nombreArchivo[50];
 };
 
 typedef struct cabecera_transaccion transaccion;
@@ -22,6 +28,12 @@ void handle_client(int sockfd);
 void send_file(int sockfd, transaccion cabecera);
 void receive_file(int sockfd, transaccion cabecera);
 
+char * unirCadenas(char * cadena1, char * cadena2);
+void ejecutarComando(char * comando);
+
+int configured;
+char configuracionPath[TAM_CONFIG_PATH];
+char * comandoCrearDirectorio = "mkdir -p ", * comandoCrearFichero = "touch ";
 int finished;
 
 
@@ -29,9 +41,10 @@ int main(int argc, char * argv[]) {
   struct sigaction act;
   struct addrinfo hints;
   struct addrinfo * info;
-  int res;
+  int res, fdConfig;
   int s;
   int c;
+  char * rutaConfig = "config.txt";
 
   //TODO Validar el archivo de configuracion
 
@@ -72,14 +85,21 @@ int main(int argc, char * argv[]) {
     perror("listen");
     exit(EXIT_FAILURE);
   }
-
+  struct stat st;
+  if (stat("config.txt", &st)) {
+    fprintf(stderr, "ERROR: No se encontró la ruta de configuración. \n");
+    exit(EXIT_FAILURE);
+  } else {
+    fdConfig = open(rutaConfig, O_RDONLY);
+    read(fdConfig, configuracionPath, TAM_CONFIG_PATH);
+    close(fdConfig);
+  }
   finished = 0;
   while (!finished) {
     //aceptar conexiones 
     printf("A la espera de conexion");//LINEA DE GUIA: BORRAR EN CUANTO SE PUEDA
     fflush(stdout);
-    c = accept(s, NULL, 0);   
-    
+    c = accept(s, NULL, 0);
     if (c < 0) {
       finished = 1;
       continue;
@@ -99,24 +119,19 @@ void handle_sigterm(int sig) {
 
 void handle_client(int sockfd) {
   printf("Handle client\n");
-  //Recibir encabezado del cliente
   //Si la operacion es "put": receive_file(sockfd);
   //Si la operacion es "get": send_file(sockfd);
   
   transaccion cabecera;
   if (read(sockfd, &cabecera, sizeof(transaccion)) != sizeof(transaccion)) {
-		fprintf(stderr, "Error al recibir encabezado");
+		fprintf(stderr, "\nError al recibir encabezado");
 		close(sockfd);
 		return;		
-  }
-  //BLOQUE DE PRUEBA
-  printf("\nTipo %d", cabecera.tipo);
-  printf("\nTamanio %d", cabecera.tamanio);
-  printf("\nRuta %s", cabecera.ruta);
-  //ELIMINAR ESTE BLOQUE
-  
+  }  
+
   switch (cabecera.tipo) {
 	  case 1:
+    printf("\nRecibiendo: %s de tamanio %d en %s", cabecera.nombreArchivo, cabecera.tamanio, cabecera.ruta);
 		receive_file(sockfd, cabecera);
 	  break;
 	  case 2:
@@ -136,5 +151,46 @@ void send_file(int sockfd, transaccion cabecera) {
 }
 
 void receive_file(int sockfd, transaccion cabecera) {
-	
+  struct stat st;
+  int fdEscritura, bytesLeidos;
+	char buffer[BUFFER_SIZE];
+  char * ruta = unirCadenas(configuracionPath, cabecera.ruta);
+  char * auxComandoCreacion = unirCadenas(comandoCrearDirectorio, configuracionPath);
+  auxComandoCreacion = unirCadenas(auxComandoCreacion, cabecera.ruta);
+  if (strlen(cabecera.ruta) > 0) {
+    ruta = unirCadenas(ruta, "/");
+  }
+  ruta = unirCadenas(ruta, cabecera.nombreArchivo);
+  if (!stat(ruta, &st)) {
+    printf("el archivo recibido ya existe\n");
+    fflush(stdout);
+    exit(EXIT_SUCCESS);
+  } else {
+    ejecutarComando(auxComandoCreacion);
+  }  
+  fdEscritura = open(ruta, O_CREAT | O_WRONLY, 00660);
+  do {
+    memset(&buffer, 0, BUFFER_SIZE);
+    bytesLeidos = read(sockfd, buffer, BUFFER_SIZE);
+    if (bytesLeidos <= 0) {
+      finished = 1;
+    } else {
+      write(fdEscritura, &buffer, bytesLeidos);
+    }
+  } while (finished != 1);
+  close(fdEscritura);  
+}
+
+char * unirCadenas(char * cadena1, char * cadena2) {
+  int longitud = strlen(cadena1) + strlen(cadena2) + 1;
+  char * resultado = (char * ) malloc (sizeof(char) * longitud);
+  memset(resultado, 0, longitud);
+  strcpy(resultado, cadena1);
+  strcat(resultado, cadena2);
+  return resultado;
+}
+
+void ejecutarComando(char * comando) {
+  FILE * elemento = popen(comando, "w");
+  pclose(elemento);
 }
